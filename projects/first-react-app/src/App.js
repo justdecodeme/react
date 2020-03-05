bf99ca741d41de155f8c5a72ad9d4d04cdab96cc
Rakesh <justdecodeme@hotmail.com>
2020-03-05 22:27:30 +0530

    181. ReactJS - Introduction to JSX

--- projects/first-react-app/src/App.js
+++ projects/first-react-app/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fName = "Rakesh";
+  const lName = "Kumar";
+  const age = 26;
+
+  const getFullName = () => `${fName} ${lName}`
+
+  const inputBox = <input placeholder = {'Your Name'}/>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>First Name: {fName}</h2>
+      <h2>Last Name: {`${lName}`}</h2>
+      <h2>Full Name: {getFullName()}</h2>
+      <h3>You are {age < 100 ? 'young' : 'Immortal'}.</h3>
+
+      {inputBox}
     </div>
   );
 }
