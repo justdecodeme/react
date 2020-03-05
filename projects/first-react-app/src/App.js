dab114af643d2f6b3b118a42c67c5aac9a707c2e
Rakesh <justdecodeme@hotmail.com>
2020-03-06 01:07:48 +0530

    186. ReactJS - Passing Data to Components using Props

--- projects/first-react-app/src/App.js
+++ projects/first-react-app/src/App.js
@@ -3,10 +3,35 @@ import './App.css';
 import BlogCard from './BlogCard';
 
 function App() {
+  const blogArr = [
+    {
+      id: 1, 
+      title: 'Heading 1',
+      desc: 'description'
+    },
+    {
+      id: 2, 
+      title: 'Heading 2',
+      desc: 'description'
+    },
+    {
+      id: 3, 
+      title: 'Heading 3',
+      desc: 'description'
+    }
+  ]
+
+  const blogCards = blogArr.map((item, pos) => {
+    console.log(item, pos);
+
+    return (
+      <BlogCard key={pos} title={item.title} desc={item.desc}></BlogCard>
+    )
+  });
+
   return (
     <div className="App">
-      <BlogCard></BlogCard>
-      <BlogCard></BlogCard>
+      {blogCards}
     </div>  
   );
 }
--- projects/first-react-app/src/BlogCard.js
+++ projects/first-react-app/src/BlogCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const BlogCard = () => {
+const BlogCard = (props) => {
+    /* props.key is not mart of props object, it is used my react internaly */
+    console.log(props);
+    
     return (
         <div className="BlogCard">
-            <h3>Blog Title</h3>
-            <p>Blog Description</p>
+            <h3>{props.title}</h3>
+            <p>{props.desc}</p>
         </div>
     )
 }
