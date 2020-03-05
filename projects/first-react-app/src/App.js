aa8f36118e5bf61b14a3f0329240fe6e398f73af
Rakesh <justdecodeme@hotmail.com>
2020-03-06 00:59:47 +0530

    185. ReactJS - Creating our First React Component

--- projects/first-react-app/src/App.js
+++ projects/first-react-app/src/App.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import './App.css';
+import BlogCard from './BlogCard';
 
 function App() {
-  const blogArr = [
-    {
-      id: 1, 
-      title: 'Heading 1',
-      desc: 'description'
-    },
-    {
-      id: 2, 
-      title: 'Heading 2',
-      desc: 'description'
-    },
-    {
-      id: 3, 
-      title: 'Heading 3',
-      desc: 'description'
-    }
-  ]
-
-  const blogCards = blogArr.map((item, pos) => {
-    console.log(item, pos);
-
-    return (
-      <div className="BlogCard" key={item.id}>
-        <h3>{item.title}</h3>
-        <p>{item.desc}</p>
-      </div>
-    )
-  });
-
   return (
     <div className="App">
-      {blogCards}
+      <BlogCard></BlogCard>
+      <BlogCard></BlogCard>
     </div>  
   );
 }
