d1cac7a8cad3ccca6112665874f6475ff98c9b04
Rakesh <justdecodeme@hotmail.com>
2020-03-05 22:40:05 +0530

    182. ReactJS - JSX Behind the Scenes

--- projects/first-react-app/src/App.js
+++ projects/first-react-app/src/App.js
@@ -3,23 +3,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const fName = "Rakesh";
-  const lName = "Kumar";
-  const age = 26;
-
-  const getFullName = () => `${fName} ${lName}`
-
-  const inputBox = <input placeholder = {'Your Name'}/>;
-
   return (
-    <div className="App">
-      <h2>First Name: {fName}</h2>
-      <h2>Last Name: {`${lName}`}</h2>
-      <h2>Full Name: {getFullName()}</h2>
-      <h3>You are {age < 100 ? 'young' : 'Immortal'}.</h3>
-
-      {inputBox}
-    </div>
+    React.createElement(
+      "div",
+      {
+        className: "App"
+      },
+      React.createElement("h2", null, "JSX behind the scene!"),
+      React.createElement(
+        "div",
+        {
+          className: "blog-card"
+        },
+        React.createElement("h1", null, "Heading"),
+        React.createElement("p", null, "Content")
+      )
+    )    
   );
 }
 
