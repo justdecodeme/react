bc22199138f8ab1fc2421f31253278563856baae
Rakesh <justdecodeme@hotmail.com>
2020-03-06 19:59:40 +0530

    193. ReactJS - Passing Props to Class-based Components - 1
    component changed to class based

--- projects/React JS - A Complete Guide for Frontend Web Development/src/BlogCard.js
+++ projects/React JS - A Complete Guide for Frontend Web Development/src/BlogCard.js
@@ -1,18 +1,35 @@
 import React from 'react';
-// import {dumpLogs} from './Utils';
+import {dumpLogs} from './Utils';
 // import './BlogCard.css';
 import classes from './BlogCard.module.css';
 
-const BlogCard = (props) => {
-    /* props.key is not mart of props object, it is used my react internaly */
-    // dumpLogs(props);
-    
-    return (
-        <div className={classes.BlogCard}>
-            <h3>{props.title}</h3>
-            <p>{props.desc}</p>
-        </div>
-    )
+class BlogCard extends React.Component {
+    state = {
+        likeCount: 0
+    }
+
+    onLikeBtnClick = () => {
+        this.setState((prevState, prevProps) => {
+            return {
+                likeCount: prevState.likeCount + 1
+            }
+        })
+    }
+
+    render() {
+        dumpLogs(this.props);
+
+        return (
+            <div className={classes.BlogCard}>
+                <h3>{this.props.title}</h3>
+                <p>{this.props.desc}</p>
+                <br/>
+                <p>Like Count: {this.state.likeCount}</p>
+                <br/>
+                <button onClick={this.onLikeBtnClick}>Like</button>
+            </div>
+        )
+    }
 }
 
 export default BlogCard;
