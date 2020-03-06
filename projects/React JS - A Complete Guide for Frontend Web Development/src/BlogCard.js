0c69f625d5acd96ee8a4fab8de4563fe54b7b780
Rakesh <justdecodeme@hotmail.com>
2020-03-06 18:42:29 +0530

    188. ReactJS - Introduction to CSS Modules - using css module file

--- projects/React JS - A Complete Guide for Frontend Web Development/src/BlogCard.js
+++ projects/React JS - A Complete Guide for Frontend Web Development/src/BlogCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {dumpLogs} from './Utils';
-import './BlogCard.css';
+// import './BlogCard.css';
+import classes from './BlogCard.module.css';
 
 const BlogCard = (props) => {
     /* props.key is not mart of props object, it is used my react internaly */
     dumpLogs(props);
     
     return (
-        <div className="BlogCard">
+        <div className={classes.BlogCard}>
             <h3>{props.title}</h3>
             <p>{props.desc}</p>
         </div>
