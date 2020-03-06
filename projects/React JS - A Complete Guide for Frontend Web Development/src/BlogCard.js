694ba073a04d1d8b69d383e2974f5ec0ae2ed3f4
Rakesh <justdecodeme@hotmail.com>
2020-03-06 18:37:08 +0530

    188. ReactJS - Introduction to CSS Modules - Normal CSS file

--- projects/React JS - A Complete Guide for Frontend Web Development/src/BlogCard.js
+++ projects/React JS - A Complete Guide for Frontend Web Development/src/BlogCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {dumpLogs} from './Utils';
+import './BlogCard.css';
 
 const BlogCard = (props) => {
     /* props.key is not mart of props object, it is used my react internaly */
