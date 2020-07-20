db3f21487a438d844fd6fe127fe1656eccf5877a
Rakesh <justdecodeme@hotmail.com>
2020-07-20 23:09:12 +0530

    redux flow without react file added

--- reference/redux/src/containers/Counter/Counter.js
+++ reference/redux/src/containers/Counter/Counter.js
@@ -1,38 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
     state = {
-        counter: 0
-    }
+        counter: 0,
+    };
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+    counterChangedHandler = (action, value) => {
+        switch (action) {
+            case "inc":
+                this.setState((prevState) => {
+                    return { counter: prevState.counter + 1 };
+                });
                 break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+            case "dec":
+                this.setState((prevState) => {
+                    return { counter: prevState.counter - 1 };
+                });
                 break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+            case "add":
+                this.setState((prevState) => {
+                    return { counter: prevState.counter + value };
+                });
                 break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+            case "sub":
+                this.setState((prevState) => {
+                    return { counter: prevState.counter - value };
+                });
                 break;
+            default:
         }
-    }
+    };
 
-    render () {
+    render() {
         return (
             <div>
                 <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl
+                    label="Increment"
+                    clicked={() => this.counterChangedHandler("inc")}
+                />
+                <CounterControl
+                    label="Decrement"
+                    clicked={() => this.counterChangedHandler("dec")}
+                />
+                <CounterControl
+                    label="Add 5"
+                    clicked={() => this.counterChangedHandler("add", 5)}
+                />
+                <CounterControl
+                    label="Subtract 5"
+                    clicked={() => this.counterChangedHandler("sub", 5)}
+                />
             </div>
         );
     }
