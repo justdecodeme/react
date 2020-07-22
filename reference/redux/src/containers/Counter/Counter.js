2329144dce9506a995329e7d0cfb93d0d6ab4152
Rakesh <justdecodeme@hotmail.com>
2020-07-22 17:39:50 +0530

    restructuring actions

--- reference/redux/src/containers/Counter/Counter.js
+++ reference/redux/src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
-import * as actionCreators from "../../store/actions/actions";
+import * as actionCreators from "../../store/actions/index";
 
 class Counter extends Component {
     state = {
@@ -88,8 +88,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onIncrementCounter: () => dispatch(actionCreators.increament()),
-        onDecrementCounter: () => dispatch(actionCreators.decreament()),
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
         onAddCounter: () => dispatch(actionCreators.add(5)),
         onSubtractCounter: () => dispatch(actionCreators.subtract(5)),
         onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
